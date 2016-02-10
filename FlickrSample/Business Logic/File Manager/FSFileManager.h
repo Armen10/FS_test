7659717cad4f6057010949fb9bcc03b5741cebf9
Armen Hakobyan <armenh@Administrators-MacBook-Pro.local>
2016-02-10 18:36:05 +0400

    //

---
+++ FlickrSample/Business Logic/File Manager/FSFileManager.h
@@ -0,0 +1,16 @@
+//
+//  FSFileManager.h
+//  FlickrSample
+//
+//  Created by Armen Hakobyan on 10.02.16.
+//  Copyright © 2016 EGS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSFileManager : NSFileManager
+
++ (NSString *)applicationDocumentsDirectory;
+
+@end
+
